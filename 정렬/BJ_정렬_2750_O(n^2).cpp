e89f74fe9c74e3bea67b16c5339e04c39e4928fc
강우 최 <101115523+ChoiKangwoo@users.noreply.github.com>
2022-07-02 23:25:42 +0900

    Create BJ_정렬_2750_O(n^2).cpp

---
+++ 정렬/BJ_정렬_2750_O(n^2).cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+void swap(int &a, int &b)
+{
+	int temp = a;
+	a = b; b = temp;
+
+}
+int main()
+{
+	int N;
+	cin >> N;
+	int *arr = new int[N];
+
+	for (int i = 0; i < N; i++)
+		cin >> arr[i];
+
+	for (int i = 0; i < N - 1; i++)
+		for (int j = i + 1; j < N; j++)
+			if (arr[i] > arr[j])
+				swap(arr[i], arr[j]);
+
+	for (int i = 0; i < N; i++)
+		cout << arr[i] << '\n';
+	delete[] arr;
+	return 0;
+
+}
