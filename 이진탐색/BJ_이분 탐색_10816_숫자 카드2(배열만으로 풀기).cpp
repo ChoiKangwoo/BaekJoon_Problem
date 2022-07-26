f1b72e050b24f6453bb214e313788c59ed8e3f09
강우 최 <101115523+ChoiKangwoo@users.noreply.github.com>
2022-07-27 01:20:28 +0900

    Update BJ_이분 탐색_10816_숫자 카드2(배열만으로 풀기).cpp

--- 이진탐색/BJ_이분 탐색_10816_숫자 카드2(배열만으로 풀기).cpp
+++ 이진탐색/BJ_이분 탐색_10816_숫자 카드2(배열만으로 풀기).cpp
@@ -6,30 +6,10 @@ using namespace std;
 int counting[20000001] = { 0 };
 /*
 해당 숫자+10000000값을 인덱스로 삼아 입력받을 때마다 해당숫자의 counting[index]를 증가시키고
-이분탐색으로 해당 숫자가 나왔을 때 counting 값을 return 한다.
+찾고자 하는 값을 입력받음과 동시에 counting[temp+10000000]을 출력시킨다.
 */
-int search_card(vector<int> &card,int m,int start,int end)
-{
-	int mid = (start + end) / 2;	//배열의 중간 인덱스 설정
-	
 
-	if (card[mid] == m)	//중간 인덱스와 의 비교를 통해서 이분탐색을 한다.
-	{
-		return counting[m+10000000];
-	}
-	else if (start >= end)
-		return 0;
-
-	else if (card[mid] < m)
-		return search_card(card, m, mid + 1, end);
-	else if (card[mid] > m)
-		return search_card(card, m, start, mid - 1);
-	else
-		return 0;
-
-}
-	
-
+// 사용한 메모리: 83344KB		걸린시간: 316ms
 int main()
 {
 	cin.tie(NULL);
