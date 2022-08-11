5d22df128139d441981595c4c416d8957cb3f364
강우 최 <101115523+ChoiKangwoo@users.noreply.github.com>
2022-08-11 23:28:13 +0900

    Update BJ_동적 계획법2_11066_파일 합치기.cpp

--- 동적 계획법2/BJ_동적 계획법2_11066_파일 합치기.cpp
+++ 동적 계획법2/BJ_동적 계획법2_11066_파일 합치기.cpp
@@ -1,44 +1,50 @@
 #include <iostream>
-#include <vector>
+#include <cstring>
 #include <algorithm>
 
 using namespace std;
 
-long long file_combine(vector<int> &A, int start, int end,long long sum)
+int memory[500][500];
+int prefix_sum[500];
+int cost[500];
+const int temp = 999999999;	//충분히 큰 값
+
+int dp(int start, int end)
 {
-	if (start + 1 == end)	//구간 원소가 두 개인 경우
-		return A[start] + A[end];
-	if (start == end)
-		return 0;
-	int left = start-1;
-	int right = end+1;
-	
-	long long half = sum / 2;
-	long long sl = 0;
-	long long sr = 0;
-	long long cost = 0;
-	while (sl < half)
-		sl += A[++left];
-	
+	if (start == end)	//재귀종료조건: 구간크기가 1일 경우는 해당 파일 비용과 같으므로 바로 return 한다.
+		return cost[start];
 
-	while (sr < half)
-		sr += A[--right];
+	int& ret = memory[start][end];
+	if (ret != -1)	return ret;		//memory를 불러와서 이미 값이 저장되어 있으면 별도의 계산필요없이 바로 return 한다.
 
+	ret = temp;	//최소값 비교를 위한 임시값 초기화
 
-	if (left == right)
-	{
-		if (sl < sr)
-			sr -= A[right++];
-		else
-			sl -= A[left--];
-	}
+	int sum;
+	if (start != 0)
+		sum = prefix_sum[end] - prefix_sum[start - 1];	// start~end 의 구간합은 0~end 구간합 - 0~(start-1) 구간합이다.
+	else
+		sum = prefix_sum[end];	//start가 0 일경우 sum은 prefix_sum[end] 와 동일하다.
 
-	cost += sl + file_combine(A, start, left, sl);
-	cost += sr + file_combine(A, right, end, sr);
+	for (int i = start; i < end; i++)
+		ret = min(ret, dp(start, i) + dp(i + 1, end) + sum);	//파일을 합치기 전의 비용과 sum의 합이 파일을 합치는데 필요한 총 비용이다.
 
-	return cost;
+	return ret;
 
 }
+
+void solve(int K)
+{
+	int ret = temp;
+
+	//구간합이 더 작은 경우를 찾을 때마다 ret 값을 갱신하면서 결과적으로는 최소값을 갖게된다.
+	for (int i = 0; i < K - 1; i++)
+		//최종 파일비용들의 합을 계산한다. 이 단계에서는 sum 더해줄 필요 없음!! 이미 return 받은 dp 값 안에 sum 이 저장되어 있음
+		//sum1+sum2=sum
+		ret = min(ret, dp(0, i) + dp(i + 1, K - 1));
+
+	cout << ret << '\n';
+}
+
 
 int main()
 {
@@ -48,17 +54,26 @@ int main()
 
 	while (T--)
 	{
+		memset(memory, -1, sizeof(memory)); //memory 비워주는 작업
+
 		int K;
 		cin >> K;
-		long long sum = 0;
-		vector<int> A(K);
+
 		for (int i = 0; i < K; i++)
+			cin >> cost[i];
+
+		for (int j = 0; j < K; j++)
 		{
-			cin >> A[i];
-			sum += A[i];
+			if (j != 0)
+				prefix_sum[j] = prefix_sum[j - 1] + cost[j];	//점화식: 0~j 번째 파일 까지의 비용 합= 0~j-1 까지 합+j 번째 파일 비용
+
+			else
+				prefix_sum[j] = cost[0];	//첫 원소는 이전원소가 존재하지 않으므로 예외를 두어 계산
 		}
 
-		cout << file_combine(A, 0, K - 1, sum) << '\n';
+		solve(K);
+
 	}
 
+	return 0;
 }
