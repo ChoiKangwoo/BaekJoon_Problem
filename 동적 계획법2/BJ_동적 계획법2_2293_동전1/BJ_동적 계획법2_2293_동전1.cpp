49e0aa06abb0d1d21d000a166ec678625cea44a4
강우 최 <101115523+ChoiKangwoo@users.noreply.github.com>
2022-08-17 00:10:58 +0900

    update

--- 동적 계획법2/BJ_동적 계획법2_2293_동전1/BJ_동적 계획법2_2293_동전1.cpp
+++ 동적 계획법2/BJ_동적 계획법2_2293_동전1/BJ_동적 계획법2_2293_동전1.cpp
@@ -4,26 +4,28 @@
 using namespace std;
 
 int coin[100];
-int memory[10000];
-int n,k;
+int memory[10001];
+int n, k;
 
 void solve()
 {
-	
-	//0의 가치를 만들어내는 경우의 수는 오직 한 개
+
+	//0의 가치를 만들어내는 경우의 수는 오직 한 개(base condition)
 	memory[0] = 1;
-	
+
+	//memory[j] 의 경우의 수는 memory[j-coin[i]]의 경우의 수들을 더해준 것과 같다.
 	for (int i = 0; i < n; i++)
 		for (int j = coin[i]; j <= k; j++)
 			memory[j] += memory[j - coin[i]];
 
+	//정답 출력
 	cout << memory[k];
 
 
 }
 int main()
 {
-	
+
 	cin >> n >> k;
 
 	fill_n(&memory[0], 10000, 0);
@@ -32,5 +34,5 @@ int main()
 		cin >> coin[i];
 
 	solve();
-	
+
 }
