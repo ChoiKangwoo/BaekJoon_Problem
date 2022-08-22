00ff6d02d499304df65de694de29f3720483dd08
강우 최 <101115523+ChoiKangwoo@users.noreply.github.com>
2022-08-22 21:03:21 +0900

    Create BJ_그래프와 순회_24444_알고리즘 수업-너비 우선 탐색1.cpp

---
+++ 그래프와 순회/BJ_그래프와 순회_24444_알고리즘 수업-너비 우선 탐색1.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <algorithm>
+#include<vector>
+#include <queue>
+
+using namespace std;
+
+vector <int> graph[100001];
+int visited[100001];
+int ans[100001];
+int c = 0;
+
+void bfs(int R)
+{
+	queue<int> q;
+	q.push(R);
+
+	visited[R] = true;
+
+	while (!q.empty())
+	{
+		int x = q.front();
+		q.pop();
+
+		ans[x] = ++c;
+		for (int i = 0; i < graph[x].size(); i++)
+		{
+			int y = graph[x][i];
+
+			if (!visited[y])
+			{
+				q.push(y);
+				visited[y] = true;
+			}
+		}
+	}
+}
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N, M, R;
+	cin >> N >> M >> R;
+
+	int u, v;
+	for (int i = 0; i < M; i++)
+	{
+		cin >> u >> v;
+		graph[u].push_back(v);
+		graph[v].push_back(u);
+	}
+
+	for (int i = 1; i <= N; i++)
+		sort(graph[i].begin(), graph[i].end());
+
+	bfs(R);
+
+	for (int i = 1; i <= N; i++)
+		cout << ans[i] << '\n';
+}
